9435c3820e87e7704ba13afd5e4c6ec680372680
Sergio Barreras <sergiobg.isc@gmail.com>
2024-03-28 19:59:40 -0700

    Menú superior

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css',
+})
+export class MenuComponent implements OnInit {
+  public items: MenuItem[] = [];
+
+  //#region Ciclo de vida de Angular
+  ngOnInit(): void {
+    this.items = [
+      { label: 'New', icon: 'pi pi-fw pi-plus' },
+      { label: 'Open', icon: 'pi pi-fw pi-download' },
+      { label: 'Undo', icon: 'pi pi-fw pi-refresh' },
+    ];
+  }
+  //#endregion
+}
